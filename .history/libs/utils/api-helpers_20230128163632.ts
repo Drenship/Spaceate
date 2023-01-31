da8e3b60cab563f7ff9c52c8b51060c5ed040ad9
Drenship <florentin.greneche@gmail.com>
2023-01-31 21:42:51 +0100

    update

--- .history/libs/utils/api-helpers_20230128163632.ts
+++ .history/libs/utils/api-helpers_20230128163632.ts
@@ -3,10 +3,7 @@ export async function fetchGetJSON(url: string) {
     const data = await fetch(url).then((res) => res.json());
     return data;
   } catch (err) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
-    throw err;
+    throw new Error(err.message);
   }
 }
 
