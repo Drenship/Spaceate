01cc0a1ad7512e734dfe539246f0c33afd9902be
Drenship <florentin.greneche@gmail.com>
2023-01-31 21:05:32 +0100

    try

--- .history/components/Wrapper/AdminscreenWrapper_20230127011027.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AdminscreenWrapper() {
-  return (
-    <div>AdminscreenWrapper</div>
-  )
-}
--- .history/components/Wrapper/AuthscreenWrapper_20230124152434.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AuthscreenWrapper() {
-    return (
-        <div>AuthscreenWrapper</div>
-    )
-}
