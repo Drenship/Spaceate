d977e536cb56fe3b56ce2cd611034fc5f975d379
Drenship <104224965+Drenship@users.noreply.github.com>
2023-01-31 22:14:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import BasescreenWrapper from '@components/Wrapper/BasescreenWrapper'
 import Productcard from '@components/cards/product-card'
-import Highlight from '@components/contents/highlights'
-import Faq from '@components/contents/Faq'
-import BestSeller from '@components/contents/BestSeller'
 
 import database from "@devasset/database.json"
 
@@ -15,8 +12,6 @@ const Home = () => {
   return (
     <BasescreenWrapper title="Accueil">
 
-      <BestSeller products={[...products, ...products]} />
-
       <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
           {
@@ -27,10 +22,6 @@ const Home = () => {
           }
         </div>
       </div>
-
-      <Faq />
-
-      <Highlight />
 
     </BasescreenWrapper>
   )
