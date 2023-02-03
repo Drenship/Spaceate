833589033f96b26bcc6c34d579f837d8a5b1d0fd
Drenship <104224965+Drenship@users.noreply.github.com>
2023-02-03 20:12:06 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NODE_ENV === 'production',
+  },
   images: {
     domains: ['rb.gy', 'pbs.twimg.com'],
   },
