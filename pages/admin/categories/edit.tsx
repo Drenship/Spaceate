779e6f289c8454998313a81443a62504a366e9bb
Drenship <florentin.greneche@gmail.com>
2023-02-04 22:33:26 +0100

    models js to ts

--- pages/admin/categories/edit.tsx
+++ pages/admin/categories/edit.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next/types';
 import React, { useMemo } from 'react';
 import { useRouter } from 'next/router';
-import AdminscreenWrapper from '@components/Wrapper/AdminscreenWrapper'
+import Link from 'next/link';
 
+import AdminscreenWrapper from '@components/Wrapper/AdminscreenWrapper'
 import InputText from '@components/ui-ux/inputs/InputText';
 import InputSelect from '@components/ui-ux/inputs/InputSelect';
 
@@ -19,14 +20,28 @@ const EditCartegorieScreen: NextPage = () => {
         }
     }, [router.query]);
 
+    const handleSubmitCartegorie = async (e: React.BaseSyntheticEvent) => {
+        e.preventDefault();
+        try {
+            const data = new FormData(e.target)
+            for (const pair of data.entries()) {
+                console.log(pair)
+            }
+            
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <AdminscreenWrapper title="Edit categorie">
 
-            <div className="mt-10 px-7">
-                <p className="text-xl font-semibold leading-tight text-gray-800">Meta Details: {query.id}</p>
+            <form onSubmit={handleSubmitCartegorie}>
+                <div className="mt-10 px-7">
+                    <p className="text-xl font-semibold leading-tight text-gray-800">Categorie Details: {query.id}</p>
 
-                <div className="grid w-full grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-7 mt-7 ">
-                    {/*<InputSelect
+                    <div className="grid w-full grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-7 mt-7 ">
+                        {/*<InputSelect
                         title="Catégorie"
                         description="Choisire une catégorie"
                         input={{
@@ -36,37 +51,44 @@ const EditCartegorieScreen: NextPage = () => {
                         options={[{ name: "a" }, { name: "b" }]}
                     /> */}
 
-                    <InputText
-                        title="Nom"
-                        description="Nom de la catégorie"
-                        input={{
-                            name: "categorie_name",
-                            defaultValue: "x",
-                            placeholder: "p",
-                        }}
-                    />
+                        <InputText
+                            title="Nom"
+                            description="Nom de la catégorie"
+                            input={{
+                                name: "name",
+                                defaultValue: "",
+                                placeholder: "entrer un nom ...",
+                            }}
+                        />
 
-                    <InputText
-                        title="Slug"
-                        description="Slug de la catégorie"
-                        input={{
-                            name: "categorie_slug",
-                            defaultValue: "x",
-                            placeholder: "p",
-                        }}
-                    />
+                        <InputText
+                            title="Slug"
+                            description="Slug de la catégorie"
+                            input={{
+                                name: "slug",
+                                defaultValue: "",
+                                placeholder: "entrer un slug ...",
+                            }}
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <hr className="h-[1px] bg-gray-100 my-14" />
-            <div className="flex flex-col flex-wrap items-center justify-center w-full px-7 lg:flex-row lg:justify-end md:justify-end gap-x-4 gap-y-4">
-                <button className="bg-white border-indigo-700 rounded hover:bg-gray-50 transform duration-300 ease-in-out text-sm font-medium px-6 py-4 text-indigo-700 border lg:max-w-[95px]  w-full ">
-                    Cancel
-                </button>
-                <button className="bg-indigo-700 rounded hover:bg-indigo-600 transform duration-300 ease-in-out text-sm font-medium px-6 py-4 text-white lg:max-w-[144px] w-full ">
-                    Save Changes
-                </button>
-            </div>
+                <hr className="h-[1px] bg-gray-100 my-14" />
+                <div className="flex flex-col flex-wrap items-center justify-center w-full px-7 lg:flex-row lg:justify-end md:justify-end gap-x-4 gap-y-4">
+                    <Link
+                        href='/admin/categories'
+                        className="bg-white border-indigo-700 rounded hover:bg-gray-50 transform duration-300 ease-in-out text-sm font-medium px-6 py-4 text-indigo-700 border lg:max-w-[95px] w-full"
+                    >
+                        Annuler
+                    </Link>
+                    <button
+                        type='submit'
+                        className="bg-indigo-700 rounded hover:bg-indigo-600 transform duration-300 ease-in-out text-sm font-medium px-6 py-4 text-white lg:max-w-[144px] w-full"
+                    >
+                        Mettre a jour
+                    </button>
+                </div>
+            </form>
 
         </AdminscreenWrapper>
     );
