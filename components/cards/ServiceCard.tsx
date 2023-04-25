0d8e877ca8b8201ec3aeb6ade71c43e9db148c20
Drenship <florentin.greneche@gmail.com>
2023-04-25 19:16:16 +0200

    home page update

--- components/cards/ServiceCard.tsx
+++ components/cards/ServiceCard.tsx
@@ -16,12 +16,12 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ service }) => {
     return (
         <Link href={service.link} className='flex flex-col z-10 md:max-w-[350px] max-w-[80vw] min-w-[320px] w-full rounded-lg shadow-lg bg-white'>
             <div className='w-full '>
-                <div className='w-full overflow-hidden rounded-lg'>
+                <div className='relative w-full overflow-hidden rounded-lg'>
                     <img src={service.src} alt={service.title} className="object-cover w-full aspect-w-142 aspect-h-199 aspect-[2/3] min-w-[280px]" />
+                    <div className='absolute top-0 bottom-0 w-full h-full' />
                 </div>
                 <div className='p-4'>
                     <h2 className='text-2xl font-bold'>{service.title}</h2>
-
                 </div>
             </div>
         </Link>
